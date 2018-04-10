151158f80c83b5bd85ccdb86e123f4c411a6a4e6
Ivan Zaytcev <izaicev@htc-cs.ru>
2018-04-10 13:20:12 +0400

    Удаление аттрибута label из формы LoginForm, добавление placeholder

--- src/AppBundle/Form/LoginForm.php
+++ src/AppBundle/Form/LoginForm.php
@@ -6,6 +6,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginForm extends AbstractType
@@ -13,8 +14,8 @@ class LoginForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('_username')
-            ->add('_password', PasswordType::class)
+            ->add('_username', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'Email']])
+            ->add('_password', PasswordType::class, ['label' => false, 'attr' => ['placeholder' => 'Password']])
             ;
     }
 }
