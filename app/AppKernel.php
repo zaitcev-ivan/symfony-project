985a24c4019a7fb6e36425f953724db8908e5b1f
Ivan Zaytcev <izaicev@htc-cs.ru>
2018-11-17 23:20:29 +0400

    Установка и подключение ElasticSearch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new SbS\AdminLTEBundle\SbSAdminLTEBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
             new AppBundle\AppBundle(),
         ];
 
